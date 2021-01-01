acb36ea86da153ae79df0589b0381b48693af381
ahnheetae <ahnheetae@ucomp.co.kr>
2021-01-02 03:09:39 +0900

    edit

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -9,8 +9,6 @@ const Main = () => {
 
   const [text, setText] = useState("");
   const [dbText, setDbText] = useState([]);
-  const [editClick, setEditClick] = useState(false);
-  const [editText, setEditText] = useState(dbText.text);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -49,6 +47,67 @@ const Main = () => {
     });
   }, []);
 
+  // const onDelete = (textId) => {
+  //   const deleteOk = window.confirm("삭제 하시겠습니까?");
+
+  //   if (deleteOk) {
+  //     dbService.doc(`send Message/${textId}`).delete();
+  //   }
+  // };
+
+  // const onEditClick = () => {
+  //   setEditClick((prev) => !prev);
+  // };
+
+  // const editChange = (e) => {
+  //   const {
+  //     target: { value },
+  //   } = e;
+  //   setEditText(value);
+  // };
+
+  // const lists = dbText.map((item) => {
+  //   return (
+  //     <>
+  //       <li style={{ margin: "10px 0" }} key={`${item.createdAt}`}>
+  //         {editClick ? <input type="text" value={editText} /> : item.text}
+  //       </li>
+
+  //       {userAuth.uid === item.createID && (
+  //         <>
+  //           <button onClick={() => onDelete(item.id)}>Delete</button>
+  //           <button onClick={onEditClick}>Edit</button>
+  //         </>
+  //       )}
+  //     </>
+  //   );
+  // });
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <input type="submit" value="send" />
+      </form>
+      <ul>
+        {dbText.map((item) => (
+          <MessageLists
+            key={item.id}
+            obj={item}
+            isOwner={userAuth.uid === item.createID}
+          />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Main;
+
+const MessageLists = ({ obj, isOwner }) => {
+  const [editClick, setEditClick] = useState(false);
+  const [editText, setEditText] = useState(obj.text);
+
   const onDelete = (textId) => {
     const deleteOk = window.confirm("삭제 하시겠습니까?");
 
@@ -68,32 +127,34 @@ const Main = () => {
     setEditText(value);
   };
 
-  const lists = dbText.map((item) => {
-    return (
-      <>
-        <li style={{ margin: "10px 0" }} key={`${item.createdAt}`}>
-          {editClick ? <input type="text" value={editText} /> : item.text}
-        </li>
-
-        {userAuth.uid === item.createID && (
-          <>
-            <button onClick={() => onDelete(item.id)}>Delete</button>
-            <button onClick={onEditClick}>Edit</button>
-          </>
-        )}
-      </>
-    );
-  });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dbService.doc(`send Message/${obj.id}`).update({ text: editText });
+    setEditClick((prev) => !prev);
+  };
 
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <input type="text" value={text} onChange={onChange} />
-        <input type="submit" value="send" />
-      </form>
-      <ul>{lists}</ul>
+      <div>
+        {editClick ? (
+          <>
+            <form onSubmit={onSubmit}>
+              <input type="text" value={editText} onChange={editChange} />{" "}
+              <button onClick={onEditClick}>Cancel</button>
+              <input type="submit" value="edit mes" />
+            </form>
+          </>
+        ) : (
+          obj.text
+        )}
+      </div>
+
+      {isOwner && (
+        <>
+          <button onClick={() => onDelete(obj.id)}>Delete</button>
+          <button onClick={onEditClick}>Edit</button>
+        </>
+      )}
     </>
   );
 };
-
-export default Main;
